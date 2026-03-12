1183c06454b49ddefe78714f262e66b3b76fbea1
pollengun <samriddhadhital@gmail.com>
2026-03-12 15:00:51 +0545

    added specific path on dir

--- icas_website/settings.py
+++ icas_website/settings.py
@@ -36,7 +36,10 @@ ROOT_URLCONF = 'icas_website.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [
+            BASE_DIR / 'templates',
+            BASE_DIR / 'icas' / 'templates',
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
